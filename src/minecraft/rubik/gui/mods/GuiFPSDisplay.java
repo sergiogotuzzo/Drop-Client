55858eefec5140a1a119f853dd344315105da694
XenKys <sergiotuzzo@gmail.com>
2024-04-07 14:58:11 +0200

    refactor: changed the button positions of fps display settings gui

--- src/minecraft/rubik/gui/mods/GuiFPSDisplay.java
+++ src/minecraft/rubik/gui/mods/GuiFPSDisplay.java
@@ -23,9 +23,9 @@ public class GuiFPSDisplay extends GuiScreen {
         
         int i = -16;
  
-        this.buttonList.add(new GuiButton(1, this.width / 2 - 100, this.height / 4 + 24 + i, 98, 20, I18n.format(mod.isEnabled() ? "§aEnabled" : "§cDisabled", new Object[0])));
-        this.buttonList.add(new GuiButton(2, this.width / 2 + 2, this.height / 4 + 24 + i, 98, 20, I18n.format((mod.isBackgroundEnabled() ? "§a" : "§c") + "Show Background", new Object[0])));
-        this.buttonList.add(new GuiButton(3, this.width / 2 - 100 + 50, this.height / 4 + 48 + i, 98, 20, I18n.format((mod.isShadowEnabled() ? "§a" : "§c") + "Text Shadow", new Object[0])));
+        this.buttonList.add(new GuiButton(1, this.width / 2 - 100 + 50, this.height / 4 + 24 + i, 98, 20, I18n.format(mod.isEnabled() ? "§aEnabled" : "§cDisabled", new Object[0])));
+        this.buttonList.add(new GuiButton(2, this.width / 2 - 100, this.height / 4 + 48 + i, 98, 20, I18n.format((mod.isBackgroundEnabled() ? "§a" : "§c") + "Show Background", new Object[0])));
+        this.buttonList.add(new GuiButton(3, this.width / 2 + 2, this.height / 4 + 48 + i, 98, 20, I18n.format((mod.isShadowEnabled() ? "§a" : "§c") + "Text Shadow", new Object[0])));
         this.buttonList.add(new GuiButton(0, this.width / 2 - 100, this.height / 6 + 168, I18n.format("gui.done", new Object[0])));
     }
 
