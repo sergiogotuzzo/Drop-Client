1828e7a2c5017cab3affe772060890c4bd31c40a
XenKys <sergiotuzzo@gmail.com>
2024-04-24 19:09:15 +0200

    refactor: delete fullbright settings gui

--- src/minecraft/rubik/gui/GuiModsList.java
+++ src/minecraft/rubik/gui/GuiModsList.java
@@ -6,7 +6,6 @@ import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.client.resources.I18n;
 import rubik.gui.mods.GuiFreelook;
-import rubik.gui.mods.GuiFullbright;
 import rubik.gui.mods.armorstatus.GuiArmorStatus;
 import rubik.gui.mods.coordinatesdisplay.GuiCoordinatesDisplay;
 import rubik.gui.mods.cpsdisplay.GuiCPSDisplay;
@@ -38,7 +37,7 @@ public class GuiModsList extends GuiScreen {
         this.buttonList.add(new GuiButton(5, this.width / 2 - 100 + j, this.height / 4 + 48 + i, 98, 20, I18n.format("FPS Display", new Object[0])));
         this.buttonList.add(new GuiButton(6, this.width / 2 + 2 + j, this.height / 4 + 48 + i, 98, 20, I18n.format("Ping Display", new Object[0])));
         this.buttonList.add(new GuiButton(7, this.width / 2 - 200 - 2 + j, this.height / 4 + 72 + i, 98, 20, I18n.format("Coming Soon...", new Object[0])));
-        this.buttonList.add(new GuiButton(8, this.width / 2 - 100 + j, this.height / 4 + 72 + i, 98, 20, I18n.format("Fullbright", new Object[0])));
+        this.buttonList.add(new GuiButton(8, this.width / 2 - 100 + j, this.height / 4 + 72 + i, 98, 20, I18n.format("Coming Soon...", new Object[0])));
         this.buttonList.add(new GuiButton(9, this.width / 2 + 2 + j, this.height / 4 + 72 + i, 98, 20, I18n.format("Freelook", new Object[0])));
         this.buttonList.add(new GuiButton(10, this.width / 2 - 100 - 26, this.height / 4 + 96 + i, 124, 20, I18n.format("Coordinates Display", new Object[0])));
         this.buttonList.add(new GuiButton(11, this.width / 2 + 2 + 0, this.height / 4 + 96 + i, 124, 20, I18n.format("Toggle Sprint / Sneak", new Object[0])));
@@ -74,7 +73,6 @@ public class GuiModsList extends GuiScreen {
             case 7:
             	break;
             case 8:
-            	this.mc.displayGuiScreen(new GuiFullbright(this));
             	break;
             case 9:
             	this.mc.displayGuiScreen(new GuiFreelook(this));
