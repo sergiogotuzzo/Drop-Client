0a3fd669f5f04897019322a7cf36552aa8a4d853
XenKys <sergiotuzzo@gmail.com>
2024-04-28 18:00:55 +0200

    refactor: disabled transparent background in chat mod

--- src/minecraft/rubik/mods/impl/Chat.java
+++ src/minecraft/rubik/mods/impl/Chat.java
@@ -3,7 +3,7 @@ package rubik.mods.impl;
 import rubik.mods.Mod;
 
 public class Chat extends Mod {
-	private boolean transparentBackground = true;
+	private boolean transparentBackground = false;
 	
 	public void setTransparentBackground(boolean enabled) {
 		transparentBackground = enabled;
