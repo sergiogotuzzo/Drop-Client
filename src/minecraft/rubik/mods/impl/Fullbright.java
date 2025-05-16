526214dd4d14b75ba5f12fb86371b0885cfa9a55
XenKys <sergiotuzzo@gmail.com>
2025-05-16 12:12:09 +0200

    fix: fullbright change gammaSetting

--- src/minecraft/rubik/mods/impl/Fullbright.java
+++ src/minecraft/rubik/mods/impl/Fullbright.java
@@ -2,26 +2,4 @@ package rubik.mods.impl;
 
 import rubik.mods.Mod;
 
-public class Fullbright extends Mod {
-	private float fullbrightGamma = 10.0F;
-	private float defaultGamma = 1.0F;
-	
-	public Fullbright() {
-		if (isEnabled()) {
-			mc.gameSettings.gammaSetting = fullbrightGamma;
-		} else {
-			mc.gameSettings.gammaSetting = defaultGamma;
-		}
-	}
-	
-	@Override
-	public void setEnabled(boolean enabled) {
-		super.setEnabled(enabled);
-		
-		if (enabled) {
-			mc.gameSettings.gammaSetting = fullbrightGamma;
-		} else {
-			mc.gameSettings.gammaSetting = defaultGamma;
-		}
-	}
-}
+public class Fullbright extends Mod {}
