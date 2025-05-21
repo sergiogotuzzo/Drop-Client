aaa6a925ee41fd2f86be4657ec76bb9600b1abe1
XenKys <sergiotuzzo@gmail.com>
2025-05-21 21:16:30 +0200

    refactor: deleted useless functions

--- src/minecraft/rubik/mods/ModDraggable.java
+++ src/minecraft/rubik/mods/ModDraggable.java
@@ -105,12 +105,4 @@ public abstract class ModDraggable extends Mod implements IRenderer {
 	    
 	    return positionX;
 	}
-	
-	public final int getLineOffset(ScreenPosition pos, int lineNum) {
-		return pos.getAbsoluteY() + getLineOffset(lineNum);
-	}
-	
-	private int getLineOffset(int lineNum) {
-		return (font.FONT_HEIGHT + 3) * lineNum;
-	}
 }
