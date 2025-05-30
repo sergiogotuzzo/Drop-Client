19acacda8cc7642fb8752324c4a86972c7a7d641
XenKys <sergiotuzzo@gmail.com>
2025-05-30 22:59:38 +0200

    refactor: hurt shake intensity min-max

--- src/minecraft/drop/gui/mod/GuiVisualTweaks.java
+++ src/minecraft/drop/gui/mod/GuiVisualTweaks.java
@@ -41,10 +41,10 @@ public class GuiVisualTweaks extends GuiDropClientScreen {
     
     @Override
     public void mouseClickMove(int mouseX, int mouseY, int clickedMouseButton, long timeSinceLastClick) {
-    	mod.setHurtShakeIntensity(sliderHurtShakeIntensity.getSliderPosition() * 20.0F);
+    	mod.setHurtShakeIntensity(sliderHurtShakeIntensity.getSliderPosition() * 35.0F);
     	
-    	if (mod.getHurtShakeIntensity() < 1.0F) {
-    		mod.setHurtShakeIntensity(1.0F);
+    	if (mod.getHurtShakeIntensity() < 5.0F) {
+    		mod.setHurtShakeIntensity(5.0F);
     	}
     }
 
@@ -67,10 +67,10 @@ public class GuiVisualTweaks extends GuiDropClientScreen {
             	this.initGui();
             	break;
             case 4:
-            	mod.setHurtShakeIntensity(sliderHurtShakeIntensity.getSliderPosition() * 20.0F);
+            	mod.setHurtShakeIntensity(sliderHurtShakeIntensity.getSliderPosition() * 35.0F);
             	
-            	if (mod.getHurtShakeIntensity() < 1.0F) {
-            		mod.setHurtShakeIntensity(1.0F);
+            	if (mod.getHurtShakeIntensity() < 5.0F) {
+            		mod.setHurtShakeIntensity(5.0F);
             	}
             	break;
         }
@@ -83,7 +83,7 @@ public class GuiVisualTweaks extends GuiDropClientScreen {
     	this.buttonList.add(new GuiButtonToggled(1, mod.isMinimalViewBobbingToggled(), (this.width + 300) / 2 - 20 - 15, (this.height - 200) / 2 + 30 + 15 * 0 + 15 - 2));
     	this.buttonList.add(new GuiButtonToggled(2, mod.isLeftHandToggled(), (this.width + 300) / 2 - 20 - 15, (this.height - 200) / 2 + 30 + 15 * 1 + 15 - 2));
     	this.buttonList.add(new GuiButtonToggled(3, mod.isHurtShakeToggled(), (this.width + 300) / 2 - 20 - 15, (this.height - 200) / 2 + 30 + 15 * 2 + 15 - 2));
-    	this.buttonList.add(sliderHurtShakeIntensity = new GuiSlider(4, (this.width - 300) / 2 + 140, (this.height - 200) / 2 + 30 + 15 * 3 + 15 + 1, 100, 5, 0, 20, mod.getHurtShakeIntensity()));
+    	this.buttonList.add(sliderHurtShakeIntensity = new GuiSlider(4, (this.width - 300) / 2 + 140, (this.height - 200) / 2 + 30 + 15 * 3 + 15 + 1, 100, 5, 5, 35, mod.getHurtShakeIntensity()));
         this.buttonList.add(new GuiButton(0, (this.width + 300) / 2 - 50 - 15, (this.height - 200) / 2 + 15, 50, 20, I18n.format("gui.done", new Object[0])));
     }
 }
