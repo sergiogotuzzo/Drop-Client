e04b615444f04bb46c6ae7127b8059165b1cd141
XenKys <sergiotuzzo@gmail.com>
2025-06-02 12:21:16 +0200

    refactor: displays ping only in multiplayer

--- src/minecraft/drop/mods/impl/PingDisplay.java
+++ src/minecraft/drop/mods/impl/PingDisplay.java
@@ -31,6 +31,34 @@ public class PingDisplay extends ModDraggableDisplayText {
 
 	@Override
 	public void render(ScreenPosition pos) {
+		if (!mc.isSingleplayer()) {
+			if (showBackground) {
+				drawRect(pos);
+			}
+			
+			int ping = mc.getNetHandler().getPlayerInfo(mc.thePlayer.getUniqueID()).getResponseTime();
+			Color dynamicColor = Color.WHITE;
+			
+			if (dynamicColors) {
+				if (ping > 300) {
+					dynamicColor = new Color(170, 0, 0);
+				} else if (ping > 200) {
+					dynamicColor = new Color(255, 85, 85);
+				} else if (ping > 150) {
+					dynamicColor = new Color(255, 170, 0);
+				} else if (ping > 100) {
+					dynamicColor = new Color(255, 255, 85);
+				} else if (ping > 50) {
+					dynamicColor = new Color(85, 255, 85);
+				}
+			}
+
+			drawCenteredText(brackets.wrap(ping + " ms"), pos.getAbsoluteX(), pos.getAbsoluteY(), dynamicColors ? dynamicColor.getRGB() : textColor.getRGB(), textShadow, textChroma && !dynamicColors);
+		}
+	}
+	
+	@Override
+	public void renderDummy(ScreenPosition pos) {
 		if (showBackground) {
 			drawRect(pos);
 		}
