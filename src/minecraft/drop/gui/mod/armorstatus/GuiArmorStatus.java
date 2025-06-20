5e5a9cc790384a80007083860b666d5f1749f09d
XenKys <sergiotuzzo@gmail.com>
2025-06-20 09:49:10 +0200

    refactor: improved armor status gui

--- src/minecraft/drop/gui/mod/armorstatus/GuiArmorStatus.java
+++ src/minecraft/drop/gui/mod/armorstatus/GuiArmorStatus.java
@@ -21,18 +21,19 @@ public class GuiArmorStatus extends GuiMod {
 
         this.writeOptionText("Text Color", 1);
         this.writeOptionText("Text Shadow", 2);
-    	this.writeOptionText("Show Percentage", 3);
     	
-    	if (!mod.isShowPercentageToggled()) {
+    	if (!mod.isShowPercentageToggled() && !mod.isShowDamageToggled()) {
+        	this.writeOptionText("Show Percentage", 3);
             this.writeOptionText("Show Damage", 4);
+    	} else if (mod.isShowPercentageToggled() && !mod.isShowDamageToggled()) {
+        	this.writeOptionText("Show Percentage", 3);
+    	} else if (!mod.isShowPercentageToggled() && mod.isShowDamageToggled()) {
+    		this.writeOptionText("Show Damage", 3);
+            this.writeOptionText("Show Max Damage", 4);
     	}
     	
-    	if (!mod.isShowPercentageToggled() && mod.isShowDamageToggled()) {
-            this.writeOptionText("Show Max Damage", 5);
-    	}
-    	
-        this.writeOptionText("Equipped Item", mod.isShowPercentageToggled() ? 4 : mod.isShowDamageToggled() ? 6 : 5);
-        this.writeOptionText("Damage Overlays", mod.isShowPercentageToggled() ? 5 : mod.isShowDamageToggled() ? 7 : 6);
+        this.writeOptionText("Equipped Item", mod.isShowPercentageToggled() ? 4 : 5);
+        this.writeOptionText("Damage Overlays", mod.isShowPercentageToggled() ? 5 : 6);
     }
 
     @Override
@@ -76,17 +77,18 @@ public class GuiArmorStatus extends GuiMod {
 
 		this.buttonList.add(this.createGuiRect(1, mod.getTextColor().getRGB(), 1));
 		this.buttonList.add(this.createGuiCheckBox(2, mod.isTextShadowToggled(), 2));
-		this.buttonList.add(this.createGuiCheckBox(3, mod.isShowPercentageToggled(), 3));
 		
-		if (!mod.isShowPercentageToggled()) {
+		if (!mod.isShowPercentageToggled() && !mod.isShowDamageToggled()) {
+			this.buttonList.add(this.createGuiCheckBox(3, mod.isShowPercentageToggled(), 3));
 			this.buttonList.add(this.createGuiCheckBox(4, mod.isShowDamageToggled(), 4));
-    	}
-    	
-    	if (mod.isShowDamageToggled()) {
-    		this.buttonList.add(this.createGuiCheckBox(5, mod.isShowMaxDamageToggled(), 5));
+    	} else if (mod.isShowPercentageToggled() && !mod.isShowDamageToggled()) {
+    		this.buttonList.add(this.createGuiCheckBox(3, mod.isShowPercentageToggled(), 3));
+    	} else if (!mod.isShowPercentageToggled() && mod.isShowDamageToggled()) {
+    		this.buttonList.add(this.createGuiCheckBox(4, mod.isShowDamageToggled(), 3));
+			this.buttonList.add(this.createGuiCheckBox(5, mod.isShowMaxDamageToggled(), 4));
     	}
 		
-		this.buttonList.add(this.createGuiCheckBox(6, mod.isEquippedItemToggled(), mod.isShowPercentageToggled() ? 4 : mod.isShowDamageToggled() ? 6 : 5));
-		this.buttonList.add(this.createGuiCheckBox(7, mod.isDamageOverlaysToggled(), mod.isShowPercentageToggled() ? 5 : mod.isShowDamageToggled() ? 7 : 6));
+		this.buttonList.add(this.createGuiCheckBox(6, mod.isEquippedItemToggled(), mod.isShowPercentageToggled() ? 4 : 5));
+		this.buttonList.add(this.createGuiCheckBox(7, mod.isDamageOverlaysToggled(), mod.isShowPercentageToggled() ? 5 : 6));
     }
 }
