59dd2d44570993938b016e0add66ce6d9b45dacc
XenKys <sergiotuzzo@gmail.com>
2025-06-21 19:12:51 +0200

    refactor: armor status gui

--- src/minecraft/drop/gui/mod/armorstatus/GuiArmorStatus.java
+++ src/minecraft/drop/gui/mod/armorstatus/GuiArmorStatus.java
@@ -19,12 +19,14 @@ public class GuiArmorStatus extends GuiMod {
     public void drawScreen(int mouseX, int mouseY, float partialTicks) {
     	super.drawScreen(mouseX, mouseY, partialTicks);
 
-        this.writeOptionText("Text Color", 1);
-        this.writeOptionText("Text Shadow", 2);
+        if (mod.isShowPercentageToggled() || mod.isShowDamageToggled()) {
+        	this.writeOptionText("Text Color", 1);
+            this.writeOptionText("Text Shadow", 2);
+        }
     	
     	if (!mod.isShowPercentageToggled() && !mod.isShowDamageToggled()) {
-        	this.writeOptionText("Show Percentage", 3);
-            this.writeOptionText("Show Damage", 4);
+        	this.writeOptionText("Show Percentage", 1);
+            this.writeOptionText("Show Damage", 2);
     	} else if (mod.isShowPercentageToggled() && !mod.isShowDamageToggled()) {
         	this.writeOptionText("Show Percentage", 3);
     	} else if (!mod.isShowPercentageToggled() && mod.isShowDamageToggled()) {
@@ -32,8 +34,8 @@ public class GuiArmorStatus extends GuiMod {
             this.writeOptionText("Show Max Damage", 4);
     	}
     	
-        this.writeOptionText("Equipped Item", mod.isShowPercentageToggled() ? 4 : 5);
-        this.writeOptionText("Damage Overlays", mod.isShowPercentageToggled() ? 5 : 6);
+        this.writeOptionText("Equipped Item", mod.isShowPercentageToggled() ? 4 : mod.isShowDamageToggled() ? 5 : 3);
+        this.writeOptionText("Damage Overlays", mod.isShowPercentageToggled() ? 5 : mod.isShowDamageToggled() ? 6 : 4);
     }
 
     @Override
@@ -75,12 +77,14 @@ public class GuiArmorStatus extends GuiMod {
     public void initGui() {
 		super.initGui();
 
-		this.buttonList.add(this.createGuiRect(1, mod.getTextColor().getRGB(), 1));
-		this.buttonList.add(this.createGuiCheckBox(2, mod.isTextShadowToggled(), 2));
+		if (mod.isShowPercentageToggled() || mod.isShowDamageToggled()) {
+			this.buttonList.add(this.createGuiRect(1, mod.getTextColor().getRGB(), 1));
+			this.buttonList.add(this.createGuiCheckBox(2, mod.isTextShadowToggled(), 2));
+		}
 		
 		if (!mod.isShowPercentageToggled() && !mod.isShowDamageToggled()) {
-			this.buttonList.add(this.createGuiCheckBox(3, mod.isShowPercentageToggled(), 3));
-			this.buttonList.add(this.createGuiCheckBox(4, mod.isShowDamageToggled(), 4));
+			this.buttonList.add(this.createGuiCheckBox(3, mod.isShowPercentageToggled(), 1));
+			this.buttonList.add(this.createGuiCheckBox(4, mod.isShowDamageToggled(), 2));
     	} else if (mod.isShowPercentageToggled() && !mod.isShowDamageToggled()) {
     		this.buttonList.add(this.createGuiCheckBox(3, mod.isShowPercentageToggled(), 3));
     	} else if (!mod.isShowPercentageToggled() && mod.isShowDamageToggled()) {
@@ -88,7 +92,7 @@ public class GuiArmorStatus extends GuiMod {
 			this.buttonList.add(this.createGuiCheckBox(5, mod.isShowMaxDamageToggled(), 4));
     	}
 		
-		this.buttonList.add(this.createGuiCheckBox(6, mod.isEquippedItemToggled(), mod.isShowPercentageToggled() ? 4 : 5));
-		this.buttonList.add(this.createGuiCheckBox(7, mod.isDamageOverlaysToggled(), mod.isShowPercentageToggled() ? 5 : 6));
+		this.buttonList.add(this.createGuiCheckBox(6, mod.isEquippedItemToggled(), mod.isShowPercentageToggled() ? 4 : mod.isShowDamageToggled() ? 5 : 3));
+		this.buttonList.add(this.createGuiCheckBox(7, mod.isDamageOverlaysToggled(), mod.isShowPercentageToggled() ? 5 : mod.isShowDamageToggled() ? 6 : 4));
     }
 }
